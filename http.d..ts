1e1518b6f93404a998955a1b7cd3a8a82ea0a671
Castellanos, Eleazar <eleazar.castellanos@dish.com>
2024-04-13 12:51:23 -0600

    Added Http  library

---
+++ http.d..ts
@@ -0,0 +1,33 @@
+declare const Methods: {
+    CONNECT: string;
+    DELETE: string;
+    GET: string;
+    HEAD: string;
+    OPTIONS: string;
+    PATCH: string;
+    POST: string;
+    PUT: string;
+    TRACE: string;
+};
+
+declare function defaultHeaders(): Record<string, string>;
+
+declare function formDataToString(formData: FormData): string;
+
+declare function getPayload(response: Response): Promise<string | Record<string, unknown>>;
+
+declare function makeHttpGetCall(
+    url: string, options: RequestInit): Promise<Response>;
+
+declare function makeHttpDeleteCall(
+    url: string, options: RequestInit): Promise<Response>;
+
+declare function makeHttpPostCall(
+    url: string, payload: BodyInit| any, options: RequestInit): Promise<Response>;
+
+export {
+    Methods,
+    makeHttpDeleteCall,
+    makeHttpGetCall,
+    makeHttpPostCall
+};
---
+++ http.js
@@ -0,0 +1,153 @@
+const Methods = {
+    CONNECT: 'CONNECT',
+    DELETE: 'DELETE',
+    GET: 'GET',
+    HEAD: 'HEAD',
+    OPTIONS: 'OPTIONS',
+    PATCH: 'PATCH',
+    POST: 'POST',
+    PUT: 'PUT',
+    TRACE: 'TRACE'
+};
+
+/**
+ * This function will return the default headers to use with a fetch call
+ * @returns {Object} Object with default headers
+ */
+function defaultHeaders(){
+    return {};
+}
+
+/**
+ * This function will convert a FormData to a string
+ * @param {FormData} formData FormData object to convert
+    * @returns {string}
+ */
+function formDataToString(formData){
+    const formDataArray = [];
+    for(const entry of formData){
+        formDataArray.push(`${entry[0]}=${entry[1]}`);
+    }
+
+    return formDataArray.join('&');
+}
+
+/**
+ * This function will get the payload from a response
+ * @param {Respone} response Response object to get payload from
+ * @returns {Object | string} Payload from response
+ */
+async function getPayload(response){
+    let payload;
+    if(response.headers.get('Content-Type').includes('json')){
+        payload = await response.json();
+    }
+    else {
+        payload = await response.text();
+    }
+
+    return payload;
+}
+
+
+/**
+ * Function makes a HTTP call
+ * @param {string} url URL to make call against
+ * @param {RequestInit} options Options to use with call
+ * @returns {Promise<Response>}
+ */
+async function makeHttpCall(url, options) {
+    return fetch(
+        url,
+        {
+            ...options,
+            headers: {
+                ...defaultHeaders(),
+                ...options?.headers
+            }
+        }
+    );
+}
+
+
+/**
+ * Function makes a HTTP GET call
+ * @param {string} url URL to make call against
+ * @param {RequestInit} options Options to use with call
+ * @returns {Promise<Response>}
+ */
+async function makeHttpGetCall(url, options) {
+    return makeHttpCall(
+        url,
+        {
+            ...options,
+            method: Methods.GET,
+            headers: {
+                ...options?.headers
+            }
+        }
+    );
+}
+
+/**
+ * Function makes a HTTP DELETE call
+ * @param {string} url URL to make call against
+ * @param {RequestInit} options Options to use with call
+ * @returns {Promise<Response>}
+ */
+async function makeHttpDeleteCall(url, options) {
+    return makeHttpCall(
+        url,
+        {
+            ...options,
+            method: Methods.DELETE,
+            headers: {
+                ...options?.headers
+            }
+        }
+    );
+}
+
+/**
+ * Function makes a HTTP POST call
+ * @param {string} url URL to make call against
+ * @param {BodyInit | any} payload Payload to send with request
+ * @param {RequestInit} [options={}] Options to use with call
+ * @returns {Promise<Response>}
+ */
+async function makeHttpPostCall(
+    url,
+    payload,
+    options = {}
+) {
+    let payloadToSend;
+
+    if(payload instanceof FormData){
+        payloadToSend = formDataToString(payload);
+        options.headers = {
+            ...options.headers,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+    }
+    else if(typeof payload === 'object'){
+        payloadToSend = JSON.stringify(payload);
+    }
+    else {
+        payloadToSend = payload;
+    }
+
+
+
+    return makeHttpCall(
+        url, 
+        {
+            ...options,
+            body: payloadToSend,
+            method: Methods.POST,
+            headers: {
+                ...options?.headers
+            }
+        }
+    );
+}
+
