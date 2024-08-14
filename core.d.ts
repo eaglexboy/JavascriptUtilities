cbfa2a4909c411ca991fa96719e49a6f79a1e7b6
El Cas <eagle_boy@rocketmail.com>
2024-08-14 10:48:35 -0600

    Update core.d.ts
    Changed any to unknown

--- core.d.ts
+++ core.d.ts
@@ -1,57 +1,57 @@
 /**
  * This function will check if the provided obj is empty. If white space flag set it will consider
  * the object empty if it only has white spaces
- * @param {any} obj Object to check
+ * @param {unknown} obj Object to check
  * @param {boolean} [whiteSpaceIsEmpty=false] Treat white spaces as empty
  * @returns {boolean} Result of checking if object is empty
  */
-declare function isEmpty(obj: any, whiteSpaceIsEmpty: boolean): boolean;
+declare function isEmpty(obj: unknown, whiteSpaceIsEmpty: boolean): boolean;
 
 /**
  * This function will check to see if the provided object is a Function
- * @param {any} obj Object to check if it's a function
+ * @param {unknown} obj Object to check if it's a function
  * @returns {boolean} Returns outcome of test
  */
-declare function isBoolean(potentialBoolean: any): boolean;
+declare function isBoolean(potentialBoolean: unknown): boolean;
 
 /**
  * This function will check if the provided number|string is a number
- * @param {number|string} potentialNumber 
+ * @param {unknown} potentialNumber 
  * @returns {boolean} Result of check
  */
-declare function isFunction(obj: number|string): boolean;
+declare function isFunction(obj: unknown): boolean;
 
 /**
  * This function will check if the provided obj is undefined or null
- * @param {any} obj Object to check
+ * @param {unknown} obj Object to check
  * @returns {boolean} Result of check
  */
-declare function isNullOrUndefined(obj: any): boolean;
+declare function isNullOrUndefined(obj: unknown): boolean;
 
 /**
  * This function will check if the provided value is Undefined or Null or Empty (if the
  * replaceOnEmpty flag is set) and if so return the default value
- * @param {any} self Value to check
- * @param {any} defaultValue Default Value to use if self is missing
+ * @param {T} self Value to check
+ * @param {T} defaultValue Default Value to use if self is missing
  * @param {boolean} [replaceOnEmpty=false] Flag to indicate if replace on empty should be done
- * @return {any}
+ * @return {T}
  */
-declare function selfOrDefault(self: any, defaultValue: any, replaceOnEmpty: boolean): any;
+declare function selfOrDefault<T>(self: T, defaultValue: T, replaceOnEmpty: boolean): T;
 
 /**
  * This method will check if object is Array, if not convert it to array. If value is undefined or
  * null then empty list is returned.
- * @param potentialArray Object to check if Array
- * @returns {any[]} Returns an Array
+ * @param {unknown} potentialArray Object to check if Array
+ * @returns {unknown[]} Returns an Array
  */
-declare function toArray(potentialArray: any): any[];
+declare function toArray(potentialArray: unknown): unknown[];
 
 /**
  * Will convert value to boolean
- * @param {any} booleanString
+ * @param {unknown} booleanString
  * @returns {boolean}
  */
-declare function toBoolean(booleanString: any): boolean;
+declare function toBoolean(booleanString: unknown): boolean;
 
 /**
  * Function to format a number to currency format
@@ -63,16 +63,16 @@ declare function toCurrency(moneyValue: number|string, withSymbol: boolean): str
 
 /**
  * Function to convert the provided string/number to a float
- * @param {number|string} potentialNumber Potential string/number to convert to float
+ * @param {unknown} potentialNumber Potential unknown to convert to float
  * @param {number} [defaultValue=0.0] Default value to use if potential number cannot be used
- * @returns 
+ * @returns {nunmber}
  */
-declare function toFloat(potentialNumber: number|string, defaultValue: number): number;
+declare function toFloat(potentialNumber: unknown, defaultValue: number): number;
 
 /**
  * Function to convert the provided string/number to a integer
- * @param {number|string} potentialNumber Potential string/number to convert to integer
+ * @param {unknown} potentialNumber Potential string/number to convert to integer
  * @param {number} [defaultValue=0.0] Default value to use if potential number cannot be used
- * @returns 
+ * @returns {number}
  */
 declare function toInteger(potentialNumber: number|string, defaultValue: number): number;
